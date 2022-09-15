7c3000e38ac8c0cce7b60d7230da6fd0e7175ea7
imiebaka <triple07even@gmail.com>
2022-09-15 10:19:58 +0100

    flex box

---
+++ frontEndEP2/app.js
@@ -0,0 +1,9 @@
+const firstInput =  document.querySelector(".first-input")
+const secondInput =  document.querySelector(".second-input")
+const btnClick =  document.querySelector(".btn-click")
+
+btnClick.addEventListener("click", function(){
+  console.log(firstInput.value);  
+  console.log(secondInput.value);  
+})
+
