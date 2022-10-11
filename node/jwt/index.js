595974185ea7663124a8f95c95c097ea99fa9316
imiebaka <triple07even@gmail.com>
2022-10-11 22:04:00 +0100

    11th Coct

---
+++ node/jwt/index.js
@@ -0,0 +1,60 @@
+const express = require("express")
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+
+const user = {
+    _id: Object("sdgdjfjgdjgfjgdjgfj"),
+    username: "JohnDoe",
+    password: "12324455",
+    profile: "4 mountain view drive"
+}
+
+const jwt = require("jsonwebtoken")
+
+app.get("/home", (req, res) => {
+    jwt.sign(user, "1234", (e, token) => {
+        try {
+            if (e) throw new Error(e)
+            res.json({ token });
+        } catch (e) {
+            console.error(e.message);
+            res.status(500).json({ message: e.message });
+        }
+    })
+})
+
+// app.use(validate())
+
+const validate = (req, res, next) => {
+    const token = req.headers.authorization.split(" ")[1]
+    if(token){
+        jwt.verify(token, '1234', function(err, payload) {
+            try {
+                if (err) throw new Error(err)
+                res.locals.username = payload._id
+                // req.body.siteUsername = payload.username
+                // req.body.username = payload.username
+                // console.log(payload.username) 
+                next()
+            } catch (e) {
+                console.error(e.message);
+                res.status(500).json({ message: e.message });
+            }
+          });
+    }
+}
+
+app.get("/protected", validate, (req, res) => {
+    console.log(res.locals.username);
+    res.status(200).json({
+        account: 937974983433,
+        name: res.locals.username
+    })
+})
+
+app.listen(2000, () => console.log("App is running"))
+// const token = jwt.sign(user, "1234")
+// console.log(token);
--- node/simpleServer/db.js
+++ node/simpleServer/db.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/user').then(() => {
-    console.log("The connection is open");
-}).catch(err => {
-    console.log(err);
-})
 
 
-module.exports = mongoose
+const db = async () => {
+    try {
+        const mongoDB = "mongodb://localhost:27017/user";
+        const connect = await mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+        console.log("Connection is open for business");
+        return connect
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+db()
 
-
-
+module.exports = db
